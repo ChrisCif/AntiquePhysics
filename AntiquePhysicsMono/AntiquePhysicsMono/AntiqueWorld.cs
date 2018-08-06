59d0f30a427e28c0d672cf416f131db119b82e4b
Christopher Cifolelli <cmc5193@g.rit.edu>
2018-08-05 22:58:53 -0400

    Antique World and Body

---
+++ AntiquePhysicsMono/AntiquePhysicsMono/AntiqueWorld.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace AntiquePhysicsMono
+{
+    class AntiqueWorld
+    {
+
+        private List<Body> bodies { get; }
+        private Rectangle map { get; }
+        private Vector2 gravity { get; set; }
+        private Vector2 wind { get; set; }
+        
+        public AntiqueWorld()
+        {
+            bodies = new List<Body>();
+            map = new Rectangle(0, 0, 800, 480);
+            gravity = new Vector2(0f, 2.0f);
+            wind = new Vector2(0f, 0f);
+        }
+
+        public void Update()
+        {
+
+            Parallel.ForEach(bodies, (bod) => {
+
+                bod.Update();
+
+            });
+
+        }
+
+        public void WorldForces()
+        {
+
+            Parallel.ForEach(bodies, (bod) => {
+
+                bod.EnactForce(gravity);
+                bod.EnactForce(wind);
+
+            });
+
+        }
+
+    }
+}
